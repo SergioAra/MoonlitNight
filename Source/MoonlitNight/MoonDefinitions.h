2c10a556382917adb04d54614e8584f0b7f8c327
SergioAra <47898195+SergioAra@users.noreply.github.com>
2025-05-27 20:44:33 -0500

    First p7

---
+++ Source/MoonlitNight/MoonDefinitions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ECC_FLOOR	ECC_GameTraceChannel1
+#define ECC_WALL	ECC_GameTraceChannel2
---
+++ Source/MoonlitNight/MoonlitNight.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MoonlitNight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MoonlitNight, "MoonlitNight" );
+
+DEFINE_LOG_CATEGORY(LogMoonlitNight)
+ 
